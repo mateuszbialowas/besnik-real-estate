c4153a9e62dc075486f07e348619256e637a304d
Mateusz Bialowas <mateusz.bialowas2@gmail.com>
2022-04-16 17:23:16 +0200

    tailwind prettier

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -10,8 +10,8 @@ export default function Navbar() {
     }
   };
   return (
-    <nav className="py-9 bg-bg-light px-2">
-      <div className="container flex-wrap flex items-center mx-auto">
+    <nav className="bg-bg-light py-9 px-2">
+      <div className="container mx-auto flex flex-wrap items-center">
         <a href="/">
           <img src={logo} alt="logo" />
         </a>
@@ -23,36 +23,36 @@ export default function Navbar() {
           onClick={toggleMenu}
         >
           <span className="sr-only">Open main menu</span>
-          <FontAwesomeIcon className="w-6 h-6" icon={faBars} />
-          <FontAwesomeIcon className="hidden w-6 h-6" icon={faXmark} />
+          <FontAwesomeIcon className="h-6 w-6" icon={faBars} />
+          <FontAwesomeIcon className="hidden h-6 w-6" icon={faXmark} />
         </button>
         <div
-          className="hidden w-full md:w-auto md:ml-12 md:flex md:items-center"
+          className="hidden w-full md:ml-12 md:flex md:w-auto md:items-center"
           id="mobile-menu"
         >
-          <ul className="flex mt-4 md:mt-0 flex-col md:flex-row md:text-sm text-gray-light">
-            <li className="p-4 border-b border-gray-300 md:border-0">
+          <ul className="mt-4 flex flex-col text-gray-light md:mt-0 md:flex-row md:text-sm">
+            <li className="border-b border-gray-300 p-4 md:border-0">
               <a href="/">Home</a>
             </li>
-            <li className="p-4 border-b border-gray-300 md:border-0">
+            <li className="border-b border-gray-300 p-4 md:border-0">
               <a href="/">Features</a>
             </li>
-            <li className="p-4 border-b border-gray-300 md:border-0">
+            <li className="border-b border-gray-300 p-4 md:border-0">
               <a href="/">Service</a>
             </li>
-            <li className="p-4 border-b border-gray-300 md:border-0">
+            <li className="border-b border-gray-300 p-4 md:border-0">
               <a href="/">Listed</a>
             </li>
-            <li className="p-4 border-b border-gray-300 md:border-0">
+            <li className="border-b border-gray-300 p-4 md:border-0">
               <a href="/">Contract</a>
             </li>
           </ul>
 
-          <div className="md:ml-[100%] flex-col md:flex-row flex md:items-center whitespace-nowrap">
-            <a className="text-primary-dark font-medium p-4" href="/">
+          <div className="flex flex-col whitespace-nowrap md:ml-[100%] md:flex-row md:items-center">
+            <a className="p-4 font-medium text-primary-dark" href="/">
               Sign up
             </a>
-            <a className="font-medium text-white primary-btn p-4" href="/">
+            <a className="primary-btn p-4 font-medium text-white" href="/">
               Register
             </a>
           </div>
